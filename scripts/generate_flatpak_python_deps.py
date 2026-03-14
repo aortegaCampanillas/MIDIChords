ea227cc05725d663de408e2f3ddf294dc8fdee5d
Antonio Ortega <antonio.ortega@iescampanillas.com>
2026-03-14 15:17:11 +0100

    Add flit-core to Flatpak Python build deps

--- scripts/generate_flatpak_python_deps.py
+++ scripts/generate_flatpak_python_deps.py
@@ -21,6 +21,7 @@ class PackageSpec:
 
 SPECS = [
     PackageSpec("setuptools", "82.0.1"),
+    PackageSpec("flit-core", "3.12.0"),
     PackageSpec("packaging", "23.2"),
     PackageSpec("setuptools-scm", "9.2.2"),
     PackageSpec("altgraph", "0.17.5"),
